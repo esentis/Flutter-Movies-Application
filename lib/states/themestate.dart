73417d249a3e38023e1aae6afea2481f1c965652
esentis <esentakos@yahoo.gr>
2020-08-23 16:13:16 +0300

    Renamed file

---
+++ lib/states/themestate.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+enum ThemeSelected { dark, light }
+var selectedTheme = ThemeSelected.dark;
+
+class SetThemeState extends ChangeNotifier {
+  ThemeSelected selectedTheme;
+
+  SetThemeState({
+    this.selectedTheme,
+  });
+  void getTheme() => selectedTheme;
+
+  void toggleTheme() {
+    logger.w('Changing theme state');
+    if (selectedTheme == ThemeSelected.dark) {
+      selectedTheme = ThemeSelected.light;
+    } else {
+      selectedTheme = ThemeSelected.dark;
+    }
+    logger.w(selectedTheme);
+    notifyListeners();
+  }
+}
