9a57c2e8ae22547e4f0bcef66c132b1f436f26dc
esentis <esentakos@yahoo.gr>
2020-08-27 14:21:27 +0300

    SearchField is now togglable

--- lib/components/search_field.dart
+++ lib/components/search_field.dart
@@ -18,6 +18,7 @@ class SearchField extends StatefulWidget {
     this.buttonTextFontSize,
     this.hintTextColor,
     this.textColor,
+    this.onClose,
   });
 
   final TextEditingController controller;
@@ -30,6 +31,7 @@ class SearchField extends StatefulWidget {
   final Color shadowColor;
   final Color hintTextColor;
   final Color textColor;
+  final Function onClose;
   @override
   _SearchFieldState createState() => _SearchFieldState();
 }
@@ -39,88 +41,193 @@ class _SearchFieldState extends State<SearchField> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        TextField(
-          textAlign: TextAlign.center,
-          controller: widget.controller,
-          enableInteractiveSelection: true,
-          onSubmitted: (value) async {
-            {
-              if (value.length <= 2) {
-                Get.snackbar(
-                  '',
-                  '',
-                  borderRadius: 20,
-                  borderColor: Colors.white,
-                  borderWidth: 5,
-                  maxWidth: 350,
-                  duration: const Duration(milliseconds: 800),
-                  backgroundColor: Colors.redAccent[400].withOpacity(0.7),
-                  titleText: Text(
-                    'At least 3 characters are needed.',
+        widget.sizingInformation.isDesktop
+            ? TextField(
+                textAlign: TextAlign.center,
+                controller: widget.controller,
+                enableInteractiveSelection: true,
+                onSubmitted: (value) async {
+                  {
+                    if (value.length <= 2) {
+                      Get.snackbar(
+                        '',
+                        '',
+                        borderRadius: 20,
+                        borderColor: Colors.white,
+                        borderWidth: 5,
+                        maxWidth: 350,
+                        duration: const Duration(milliseconds: 800),
+                        backgroundColor: Colors.redAccent[400].withOpacity(0.7),
+                        titleText: Text(
+                          'At least 3 characters are needed.',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.newsCycle(
+                            fontSize:
+                                widget.sizingInformation.isMobile ? 20 : 35,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      );
+                      return;
+                    }
+                    var response = await searchMovies(value);
+
+                    await Get.toNamed('/search', arguments: response);
+                  }
+                },
+                toolbarOptions: const ToolbarOptions(
+                  copy: true,
+                  paste: true,
+                  cut: true,
+                  selectAll: true,
+                ),
+                decoration: InputDecoration(
+                    filled: true,
+                    enabledBorder: UnderlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Color(0xFFEC1E79),
+                        width: 12,
+                      ),
+                    ),
+                    disabledBorder: UnderlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                        color: Colors.red.withOpacity(0.3),
+                        width: 12,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Colors.green,
+                        width: 12,
+                      ),
+                    ),
+                    border: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: Colors.white,
+                        )),
+                    hintText: !widget.sizingInformation.isDesktop
+                        ? 'Search movie'
+                        : 'Start typing',
+                    hintStyle: GoogleFonts.newsCycle(
+                      color: widget.hintTextColor,
+                      fontSize: 25,
+                    )),
+                style: GoogleFonts.newsCycle(
+                  fontSize: widget.textFontSize,
+                  color: widget.textColor,
+                  fontWeight: FontWeight.bold,
+                ),
+                onChanged: (value) {
+                  logger.i(value);
+                },
+              )
+            : Stack(
+                children: [
+                  TextField(
                     textAlign: TextAlign.center,
+                    controller: widget.controller,
+                    enableInteractiveSelection: true,
+                    onSubmitted: (value) async {
+                      {
+                        if (value.length <= 2) {
+                          Get.snackbar(
+                            '',
+                            '',
+                            borderRadius: 20,
+                            borderColor: Colors.white,
+                            borderWidth: 5,
+                            maxWidth: 350,
+                            duration: const Duration(milliseconds: 800),
+                            backgroundColor:
+                                Colors.redAccent[400].withOpacity(0.7),
+                            titleText: Text(
+                              'At least 3 characters are needed.',
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.newsCycle(
+                                fontSize:
+                                    widget.sizingInformation.isMobile ? 20 : 35,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          );
+                          return;
+                        }
+                        var response = await searchMovies(value);
+
+                        await Get.toNamed('/search', arguments: response);
+                      }
+                    },
+                    toolbarOptions: const ToolbarOptions(
+                      copy: true,
+                      paste: true,
+                      cut: true,
+                      selectAll: true,
+                    ),
+                    decoration: InputDecoration(
+                        filled: true,
+                        enabledBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xFFEC1E79),
+                            width: 12,
+                          ),
+                        ),
+                        disabledBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(
+                            color: Colors.red.withOpacity(0.3),
+                            width: 12,
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Colors.green,
+                            width: 12,
+                          ),
+                        ),
+                        border: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: const BorderSide(
+                              width: 2,
+                              color: Colors.white,
+                            )),
+                        hintText: !widget.sizingInformation.isDesktop
+                            ? 'Search movie'
+                            : 'Start typing',
+                        hintStyle: GoogleFonts.newsCycle(
+                          color: widget.hintTextColor,
+                          fontSize: 25,
+                        )),
                     style: GoogleFonts.newsCycle(
-                      fontSize: widget.sizingInformation.isMobile ? 20 : 35,
-                      color: Colors.white,
+                      fontSize: widget.textFontSize,
+                      color: widget.textColor,
                       fontWeight: FontWeight.bold,
                     ),
+                    onChanged: (value) {
+                      logger.i(value);
+                    },
                   ),
-                );
-                return;
-              }
-              var response = await searchMovies(value);
-
-              await Get.toNamed('/search', arguments: response);
-            }
-          },
-          toolbarOptions: const ToolbarOptions(
-            copy: true,
-            paste: true,
-            cut: true,
-            selectAll: true,
-          ),
-          decoration: InputDecoration(
-              filled: true,
-              enabledBorder: UnderlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide: const BorderSide(
-                  color: Color(0xFFEC1E79),
-                  width: 12,
-                ),
+                  Positioned(
+                    right: 0,
+                    child: GestureDetector(
+                      onTap: widget.onClose,
+                      child: const Icon(
+                        Icons.close,
+                        color: Colors.red,
+                        size: 35,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              disabledBorder: UnderlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide: BorderSide(
-                  color: Colors.red.withOpacity(0.3),
-                  width: 12,
-                ),
-              ),
-              focusedBorder: UnderlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide: const BorderSide(
-                  color: Colors.green,
-                  width: 12,
-                ),
-              ),
-              border: UnderlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: const BorderSide(
-                    width: 2,
-                    color: Colors.white,
-                  )),
-              hintText: 'Start typing',
-              hintStyle: GoogleFonts.newsCycle(
-                color: widget.hintTextColor,
-                fontSize: 25,
-              )),
-          style: GoogleFonts.newsCycle(
-            fontSize: widget.textFontSize,
-            color: widget.textColor,
-            fontWeight: FontWeight.bold,
-          ),
-          onChanged: (value) {
-            logger.i(value);
-          },
-        ),
         widget.sizingInformation.isDesktop
             ? Padding(
                 padding: const EdgeInsets.only(top: 14.0),
