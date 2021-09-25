ba89315893e2547bbedb47a606daa267aa3d2432
esentis <52862600+esentis@users.noreply.github.com>
2021-09-25 23:05:58 +0300

    Enhancement/deploy on firebase (#33)
    * Updates README
    
    * Configures firebase
    
    * Updates dependencies
    
    * Fixes deprecations
    
    * Renames .env file to work on web
    
    * Configures iOS folder
    
    * Adds dotenv on gitignore
    
    * Removes dotenv
    
    * Updates .gitignore
    
    * Renames env file

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -3,14 +3,14 @@ import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:news_api/components/general/apptitle.dart';
+import 'package:news_api/models/movie_search_results.dart';
 import 'package:news_api/networking/connection.dart';
 import 'package:news_api/states/themestate.dart';
-import 'package:responsive_builder/responsive_builder.dart';
-import 'package:news_api/models/movie_search_results.dart';
-import '../constants.dart';
 import 'package:provider/provider.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+import '../constants.dart';
 
 bool _isLoading = false;
 
@@ -43,148 +43,144 @@ class _SearchResultsState extends State<SearchResults> {
           ],
         ),
       ),
-      child: ModalProgressHUD(
-        inAsyncCall: _isLoading,
-        child: SafeArea(
-          child: ResponsiveBuilder(
-            builder: (context, sizingInformation) => Scaffold(
-              backgroundColor: Colors.transparent,
-              appBar: AppBar(
-                backgroundColor: themeState.selectedTheme == ThemeSelected.dark
-                    ? const Color(0xff1a1a2e).withOpacity(0.2)
-                    : Colors.white.withOpacity(0.2),
-                elevation: 15,
-                shadowColor: themeState.selectedTheme == ThemeSelected.dark
-                    ? const Color(0xFFf7f7f7).withOpacity(0.3)
-                    : Colors.black.withOpacity(0.7),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(60),
-                    bottomLeft: Radius.circular(60),
-                  ),
-                ),
-                toolbarHeight: sizingInformation.isMobile
-                    ? sizingInformation.isTablet
-                        ? 110
-                        : 110
-                    : 140,
-                leading: Builder(
-                  builder: (BuildContext context) => IconButton(
-                    icon: Icon(
-                      Icons.arrow_back,
-                      size: 40,
-                      color: themeState.selectedTheme == ThemeSelected.dark
-                          ? const Color(0xFFf7f7f7)
-                          : Colors.black.withOpacity(0.5),
-                    ),
-                    onPressed: Get.back,
-                  ),
-                ),
-                title: AppTitle(
-                  onSearchTap: () {},
-                  searchIconColor:
-                      themeState.selectedTheme == ThemeSelected.dark
-                          ? const Color(0xFFf7f7f7)
-                          : Colors.black.withOpacity(0.5),
-                  settingsIconColor:
-                      themeState.selectedTheme == ThemeSelected.dark
-                          ? const Color(0xFFf7f7f7)
-                          : Colors.black.withOpacity(0.5),
+      child: SafeArea(
+        child: ResponsiveBuilder(
+          builder: (context, sizingInformation) => Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: themeState.selectedTheme == ThemeSelected.dark
+                  ? const Color(0xff1a1a2e).withOpacity(0.2)
+                  : Colors.white.withOpacity(0.2),
+              elevation: 15,
+              shadowColor: themeState.selectedTheme == ThemeSelected.dark
+                  ? const Color(0xFFf7f7f7).withOpacity(0.3)
+                  : Colors.black.withOpacity(0.7),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(60),
+                  bottomLeft: Radius.circular(60),
                 ),
               ),
-              body: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 8.0,
-                    vertical: 15,
+              toolbarHeight: sizingInformation.isMobile
+                  ? sizingInformation.isTablet
+                      ? 110
+                      : 110
+                  : 140,
+              leading: Builder(
+                builder: (BuildContext context) => IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    size: 40,
+                    color: themeState.selectedTheme == ThemeSelected.dark
+                        ? const Color(0xFFf7f7f7)
+                        : Colors.black.withOpacity(0.5),
                   ),
-                  child: searchResults.results.isEmpty
-                      ? Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'No results',
-                              style: GoogleFonts.newsCycle(
-                                fontSize: 45,
-                                fontWeight: FontWeight.bold,
-                                color: themeState.selectedTheme ==
-                                        ThemeSelected.dark
-                                    ? Colors.white
-                                    : const Color(0xFFEC1E79),
-                              ),
-                            ),
-                            const Expanded(
-                              child: FlareActor(
-                                'assets/animations/not_found_404.flr',
-                                alignment: Alignment.center,
-                                fit: BoxFit.contain,
-                                animation: 'idle',
-                              ),
-                            ),
-                          ],
-                        )
-                      : ListView.separated(
-                          itemCount: searchResults.results.length,
-                          itemBuilder: (context, index) => ListTile(
-                            onTap: () async {
-                              var movieCredits = await getCredits(
-                                  searchResults.results[index].id);
-                              var movieDetails = await getMovie(
-                                  searchResults.results[index].id);
-                              await Get.toNamed('/movie',
-                                  arguments: [
-                                        movieDetails,
-                                        movieCredits,
-                                        index,
-                                        'search'
-                                      ] ??
-                                      '');
-                            },
-                            leading: searchResults.results[index].posterPath !=
-                                    null
-                                ? ClipRRect(
-                                    borderRadius: BorderRadius.circular(60),
-                                    child: Image.network(
-                                      '$baseImgUrl${searchResults.results[index].posterPath}',
-                                    ),
-                                  )
-                                : Image.asset('assets/images/404_actor.png'),
-                            title: Text(
-                              searchResults.results[index].title,
-                              style: GoogleFonts.newsCycle(
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold,
-                                color: themeState.selectedTheme ==
-                                        ThemeSelected.dark
-                                    ? Colors.white
-                                    : Colors.black,
-                              ),
-                            ),
-                            subtitle: Text(
-                                formatDate(
-                                    searchResults.results[index].releaseDate,
-                                    [d, '-', M, '-', yy]),
-                                style: GoogleFonts.newsCycle(
-                                  fontSize: 15,
-                                  color: themeState.selectedTheme ==
-                                          ThemeSelected.dark
+                  onPressed: Get.back,
+                ),
+              ),
+              title: AppTitle(
+                onSearchTap: () {},
+                searchIconColor: themeState.selectedTheme == ThemeSelected.dark
+                    ? const Color(0xFFf7f7f7)
+                    : Colors.black.withOpacity(0.5),
+                settingsIconColor:
+                    themeState.selectedTheme == ThemeSelected.dark
+                        ? const Color(0xFFf7f7f7)
+                        : Colors.black.withOpacity(0.5),
+              ),
+            ),
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 15,
+                ),
+                child: searchResults.results.isEmpty
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'No results',
+                            style: GoogleFonts.newsCycle(
+                              fontSize: 45,
+                              fontWeight: FontWeight.bold,
+                              color:
+                                  themeState.selectedTheme == ThemeSelected.dark
                                       ? Colors.white
-                                      : Colors.black,
-                                )),
-                            trailing: Text(
-                              '${searchResults.results[index].voteAverage}/10',
-                              style: GoogleFonts.newsCycle(
-                                fontSize: 20,
-                                color: themeState.selectedTheme ==
-                                        ThemeSelected.dark
-                                    ? Colors.white
-                                    : Colors.black,
-                              ),
+                                      : const Color(0xFFEC1E79),
                             ),
                           ),
-                          separatorBuilder: (context, index) => const Divider(),
+                          const Expanded(
+                            child: FlareActor(
+                              'assets/animations/not_found_404.flr',
+                              alignment: Alignment.center,
+                              fit: BoxFit.contain,
+                              animation: 'idle',
+                            ),
+                          ),
+                        ],
+                      )
+                    : ListView.separated(
+                        itemCount: searchResults.results.length,
+                        itemBuilder: (context, index) => ListTile(
+                          onTap: () async {
+                            var movieCredits = await getCredits(
+                                searchResults.results[index].id);
+                            var movieDetails =
+                                await getMovie(searchResults.results[index].id);
+                            await Get.toNamed('/movie',
+                                arguments: [
+                                      movieDetails,
+                                      movieCredits,
+                                      index,
+                                      'search'
+                                    ] ??
+                                    '');
+                          },
+                          leading:
+                              searchResults.results[index].posterPath != null
+                                  ? ClipRRect(
+                                      borderRadius: BorderRadius.circular(60),
+                                      child: Image.network(
+                                        '$baseImgUrl${searchResults.results[index].posterPath}',
+                                      ),
+                                    )
+                                  : Image.asset('assets/images/404_actor.png'),
+                          title: Text(
+                            searchResults.results[index].title,
+                            style: GoogleFonts.newsCycle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color:
+                                  themeState.selectedTheme == ThemeSelected.dark
+                                      ? Colors.white
+                                      : Colors.black,
+                            ),
+                          ),
+                          subtitle: Text(
+                              formatDate(
+                                  searchResults.results[index].releaseDate,
+                                  [d, '-', M, '-', yy]),
+                              style: GoogleFonts.newsCycle(
+                                fontSize: 15,
+                                color: themeState.selectedTheme ==
+                                        ThemeSelected.dark
+                                    ? Colors.white
+                                    : Colors.black,
+                              )),
+                          trailing: Text(
+                            '${searchResults.results[index].voteAverage}/10',
+                            style: GoogleFonts.newsCycle(
+                              fontSize: 20,
+                              color:
+                                  themeState.selectedTheme == ThemeSelected.dark
+                                      ? Colors.white
+                                      : Colors.black,
+                            ),
+                          ),
                         ),
-                ),
+                        separatorBuilder: (context, index) => const Divider(),
+                      ),
               ),
             ),
           ),
